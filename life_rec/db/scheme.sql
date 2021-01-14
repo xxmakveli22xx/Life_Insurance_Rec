4a84b1fa653f8cc2d72c42aca8c41bdd8470ab87
Pete Hernandez <pete.g.hernandez@gmail.com>
2021-01-14 12:51:26 -0600

    created the link to mysql and created db

---
+++ life_rec/db/scheme.sql
@@ -0,0 +1,5 @@
+-- Drops the "Life Rec DB" if it exists currently --
+DROP DATABASE IF EXISTS LifeRec;
+-- Creates the "Life Rec DB" database --
+CREATE DATABASE LifeRec;
+
--- life_rec/server.js
+++ life_rec/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const db = require("./models");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -19,6 +20,10 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
+db.sequelize.sync().then(function(){
+  app.listen(PORT, () => {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+  });
+  
+  })
+
